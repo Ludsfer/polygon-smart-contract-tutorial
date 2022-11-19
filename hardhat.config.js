53f1ec95e8862a7ea75af499f6bac13202c5d008
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2022-11-19 19:59:14 +0000

    Testing the contract on the MATIC Mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,7 +17,7 @@
 require("@nomicfoundation/hardhat-chai-matchers");
 require("dotenv").config();
 
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, PRIVATE_KEY, MATIC_URL } = process.env;
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -31,6 +31,11 @@ module.exports = {
     mumbai: {
       url: API_URL,
       accounts: [`0x${PRIVATE_KEY}`]
+    },
+    matic: {
+      url: MATIC_URL,
+      accounts: [`0x${PRIVATE_KEY}`] // Real production Mainnet. Use the same private key if 
+      // the TEST account (TESTNET) is the same as the production account (MAINNET)
     }
   }
 };
